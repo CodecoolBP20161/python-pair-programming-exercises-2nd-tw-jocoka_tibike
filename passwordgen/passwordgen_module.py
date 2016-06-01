0acd8e80e848d2f2d00a47dd64e3694cc2810beb
Tibor Kiss <tiborkiss1008@gmail.com>
2016-06-01 14:55:29 +0200

    Passwordgen completed

--- passwordgen/passwordgen_module.py
+++ passwordgen/passwordgen_module.py
@@ -1,9 +1,29 @@
+import string
+import random
+
+
 def passwordgen():
-    return
+    size = 6
+    a = string.ascii_letters
+    b = "#" + "!" + "@" + "$"
+    c = string.digits
+    d = ''.join(random.choice(a) for i in range(size))
+    e = ''.join(random.choice(b))
+    f = ''.join(random.choice(c))
+    g = d + e + f
+    print(g)
+    return g
 
 
 def main():
-    return
+    while True:
+        n = input("This is a password generator. Type n for a new random password. Type anything else to quit.")
+        if n == "n":
+            passwordgen()
+        else:
+            break
+    return passwordgen()
+
 
 
 if __name__ == '__main__':
