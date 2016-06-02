f82a0f735e4a95d88a18bc50cc1c6ef496c94f30
Tibor Kiss <tiborkiss1008@gmail.com>
2016-06-02 15:03:22 +0200

    palindrome complete

--- palindrome/palindrome_module.py
+++ palindrome/palindrome_module.py
@@ -1,9 +1,20 @@
-def palindrome(str):
-    return
+
+
+def palindrome(word):
+    word = word.lower()
+    for i, char in enumerate(word):
+        if char != word[-i-1]:
+            return False
+        else:
+            return True
 
 
 def main():
-    return
+    word = input("Enter text to check if it's a palindrome!: ")
+    if palindrome(word):
+        print("Palindrome")
+    else:
+        print("Not a palindrome")
 
 
 if __name__ == '__main__':
