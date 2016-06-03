b06bcfc2f916fc45d32b8e0a675de3f4de231508
Tibor Kiss <tiborkiss1008@gmail.com>
2016-06-03 09:30:06 +0200

    years extra

--- years/years_module.py
+++ years/years_module.py
@@ -6,8 +6,12 @@ def years(age):
     date.year
     birth = date.year - age + 100
     print("You will be a hundred years old in {0}, if you live long enough" .format(birth))
-
+    es = ("You will be a hundred years old in " + str(birth) + ", if you live long enough")
+    ki = int(input("How many times do you want the previous line to be written?: "))
+    for i in range(ki):
+        print(es)
     return birth
+
 
 def main():
     name = input("What's your name?: ")
